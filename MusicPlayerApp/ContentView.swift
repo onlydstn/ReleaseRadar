a34b79298e2f4e71144183480bd435c0fc197eaa
Dustin Nuzzo <dustin.nuzzo@icloud.com>
2024-09-10 09:02:44 +0200

    Initial Commit

---
+++ MusicPlayerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicPlayerApp
+//
+//  Created by Dustin Nuzzo on 10.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicPlayerApp/MusicPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerAppApp.swift
+//  MusicPlayerApp
+//
+//  Created by Dustin Nuzzo on 10.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
