482a069b6305a77f7ac5930c9a0a55748210e47b
Dustin Nuzzo <dustin.nuzzo@icloud.com>
2024-09-10 09:12:11 +0200

    initial commit

---
+++ MusicPlayerApp/ChartView.swift
@@ -0,0 +1,18 @@
+//
+//  ChartView.swift
+//  MusicPlayerApp
+//
+//  Created by Dustin Nuzzo on 10.09.24.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChartView()
+}
