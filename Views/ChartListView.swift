052c3285dd329c12c85db32d3e122ecca49a6c86
Dustin Nuzzo <dustin.nuzzo@icloud.com>
2024-09-12 11:40:54 +0200

    Länderliste an Apple Music angepasst

--- Views/ChartListView.swift
+++ Views/ChartListView.swift
@@ -30,45 +30,11 @@ struct ChartListView: View {
     @State var songsArray: [ChartEntry] = [] // Array der geladene Songs speichert
     @State var selectedType = "Songs" // Songs oder Alben auswählen
     @State var selectedCountry = "de" // Standard DE
+    @State var canLoadData = false
     
     let chartTypes = ["Songs", "Alben"] // Picker für jeweiligen Typ
-    let availableCountries = [ // Dictionary mit Key und Value "de" <- Key, "Germany" <- Value
-        "af": "Afghanistan", "al": "Albanien", "dz": "Algerien", "ad": "Andorra", "ao": "Angola",
-        "ag": "Antigua und Barbuda", "ar": "Argentinien", "am": "Armenien", "az": "Aserbaidschan",
-        "bs": "Bahamas", "bh": "Bahrain", "bd": "Bangladesch", "bb": "Barbados", "be": "Belgien",
-        "bz": "Belize", "bj": "Benin", "bt": "Bhutan", "bo": "Bolivien", "ba": "Bosnien und Herzegowina",
-        "bw": "Botswana", "br": "Brasilien", "bn": "Brunei", "bg": "Bulgarien", "bf": "Burkina Faso",
-        "bi": "Burundi", "cl": "Chile", "cn": "China", "ck": "Cookinseln",
-        "cr": "Costa Rica", "de": "Deutschland", "dj": "Dschibuti", "dm": "Dominica",
-        "do": "Dominikanische Republik", "ec": "Ecuador", "sv": "El Salvador", "eh": "Erschließung",
-        "ee": "Estland", "fj": "Fidschi", "fi": "Finnland", "fr": "Frankreich", "ga": "Gabon",
-        "gm": "Gambia", "ge": "Georgien", "gh": "Ghana", "gr": "Griechenland", "gd": "Grenada",
-        "gp": "Guadeloupe", "gu": "Guam", "gt": "Guatemala", "gn": "Guinea", "gw": "Guinea-Bissau",
-        "gy": "Guyana", "hn": "Honduras", "hk": "Hongkong", "in": "Indien", "id": "Indonesien",
-        "iq": "Irak", "ie": "Irland", "is": "Island", "il": "Israel", "it": "Italien", "jm": "Jamaika",
-        "jp": "Japan", "jo": "Jordanien", "kh": "Kambodscha", "cm": "Kamerun", "ca": "Kanada",
-        "cv": "Kap Verde", "kz": "Kasachstan", "qa": "Katar", "ke": "Kenia", "kg": "Kirgisistan",
-        "ki": "Kiribati", "co": "Kolumbien", "hr": "Kroatien", "cu": "Kuba", "kw": "Kuwait",
-        "la": "Laos", "ls": "Lesotho", "lv": "Lettland", "lb": "Libanon", "lr": "Liberia",
-        "ly": "Libyen", "li": "Liechtenstein", "lt": "Litauen", "lu": "Luxemburg", "mg": "Madagaskar",
-        "mw": "Malawi", "my": "Malaysia", "mt": "Malta", "ma": "Marokko", "mr": "Mauretanien",
-        "mu": "Mauritius", "mx": "Mexiko", "md": "Moldawien", "mc": "Monaco", "mn": "Mongolei",
-        "me": "Montenegro", "mz": "Mozambik", "mm": "Myanmar", "na": "Namibia", "nr": "Nauru",
-        "np": "Nepal", "nc": "Neukaledonien", "ne": "Niger", "ng": "Nigeria", "mk": "Nordmazedonien",
-        "no": "Norwegen", "om": "Oman", "pk": "Pakistan", "ps": "Palästina", "pa": "Panama",
-        "pg": "Papua-Neuguinea", "py": "Paraguay", "pe": "Peru", "ph": "Philippinen", "pl": "Polen",
-        "pt": "Portugal", "rw": "Ruanda", "ro": "Rumänien", "sm": "San Marino", "st": "Sao Tome und Principe",
-        "sa": "Saudi-Arabien", "se": "Schweden", "ch": "Schweiz", "sn": "Senegal", "rs": "Serbien",
-        "sc": "Seychellen", "sl": "Sierra Leone", "sg": "Singapur", "sk": "Slowakei", "si": "Slowenien",
-        "sb": "Solomon-Inseln", "so": "Somalia", "es": "Spanien", "lk": "Sri Lanka", "kn": "St. Kitts und Nevis",
-        "lc": "St. Lucia", "vc": "St. Vincent und die Grenadinen", "za": "Südafrika", "ss": "Südsudan",
-        "kr": "Südkorea", "sy": "Syrien", "tj": "Tadschikistan", "tz": "Tansania", "th": "Thailand",
-        "tg": "Togo", "to": "Tonga", "tt": "Trinidad und Tobago", "tn": "Tunesien", "tr": "Türkei",
-        "tm": "Turkmenistan", "ug": "Uganda", "ua": "Ukraine", "hu": "Ungarn", "uy": "Uruguay",
-        "uz": "Usbekistan", "vu": "Vanuatu", "ve": "Venezuela", "ae": "Vereinigte Arabische Emirate",
-        "us": "Vereinigte Staaten", "gb": "Vereinigtes Königreich", "vn": "Vietnam",
-        "cf": "Zentralafrikanische Republik", "cy": "Zypern"
-    ]
+    let availableCountries = [ // Dictionary für Pickerauswahl Länder ["de": "Deutschland"] "de" <- Key, "Deutschland" <- Value
+        "dz": "Algerien", "ao": "Angola", "ai": "Anguilla", "ag": "Antigua und Barbuda", "ar": "Argentinien", "am": "Armenien", "au": "Australien", "at": "Österreich", "az": "Aserbaidschan", "bs": "Bahamas", "bh": "Bahrain", "bb": "Barbados", "by": "Weißrussland", "be": "Belgien", "bz": "Belize", "bj": "Benin", "bm": "Bermuda", "bt": "Bhutan", "bo": "Bolivien", "ba": "Bosnien und Herzegowina", "bw": "Botsuana", "br": "Brasilien", "vg": "Britische Jungferninseln", "bg": "Bulgarien", "kh": "Kambodscha", "cm": "Kamerun", "ca": "Kanada", "cv": "Kap Verde", "ky": "Kaimaninseln", "td": "Tschad", "cl": "Chile", "cn": "China Festland", "co": "Kolumbien", "cr": "Costa Rica", "hr": "Kroatien", "cy": "Zypern", "cz": "Tschechische Republik", "ci": "Elfenbeinküste", "cd": "Demokratische Republik Kongo", "dk": "Dänemark", "dm": "Dominica", "do": "Dominikanische Republik", "ec": "Ecuador", "eg": "Ägypten", "sv": "El Salvador", "ee": "Estland", "sz": "Eswatini", "fj": "Fidschi", "fi": "Finnland", "fr": "Frankreich", "ga": "Gabun", "gm": "Gambia", "ge": "Georgien", "de": "Deutschland", "gh": "Ghana", "gr": "Griechenland", "gd": "Grenada", "gt": "Guatemala", "gw": "Guinea-Bissau", "gy": "Guyana", "hn": "Honduras", "hk": "Hongkong", "hu": "Ungarn", "is": "Island", "in": "Indien", "id": "Indonesien", "iq": "Irak", "ie": "Irland", "il": "Israel", "it": "Italien", "jm": "Jamaika", "jp": "Japan", "jo": "Jordanien", "kz": "Kasachstan", "ke": "Kenia", "kr": "Südkorea", "xk": "Kosovo", "kw": "Kuwait", "kg": "Kirgisistan", "la": "Laos", "lv": "Lettland", "lb": "Libanon", "lr": "Liberia", "ly": "Libyen", "lt": "Litauen", "lu": "Luxemburg", "mo": "Macao", "mg": "Madagaskar", "mw": "Malawi", "my": "Malaysia", "mv": "Malediven", "ml": "Mali", "mt": "Malta", "mr": "Mauretanien", "mu": "Mauritius", "mx": "Mexiko", "fm": "Mikronesien", "md": "Moldawien", "mn": "Mongolei", "me": "Montenegro", "ms": "Montserrat", "ma": "Marokko", "mz": "Mosambik", "mm": "Myanmar", "na": "Namibia", "np": "Nepal", "nl": "Niederlande", "nz": "Neuseeland", "ni": "Nicaragua", "ne": "Niger", "ng": "Nigeria", "mk": "Nordmazedonien", "no": "Norwegen", "om": "Oman", "pa": "Panama", "pg": "Papua-Neuguinea", "py": "Paraguay", "pe": "Peru", "ph": "Philippinen", "pl": "Polen", "pt": "Portugal", "qa": "Katar", "cg": "Republik Kongo", "ro": "Rumänien", "ru": "Russland", "rw": "Ruanda", "sa": "Saudi-Arabien", "sn": "Senegal", "rs": "Serbien", "sc": "Seychellen", "sl": "Sierra Leone", "sg": "Singapur", "sk": "Slowakei", "si": "Slowenien", "sb": "Salomonen", "za": "Südafrika", "es": "Spanien", "lk": "Sri Lanka", "kn": "St. Kitts und Nevis", "lc": "St. Lucia", "vc": "St. Vincent und die Grenadinen", "sr": "Suriname", "se": "Schweden", "ch": "Schweiz", "tw": "Taiwan", "tj": "Tadschikistan", "tz": "Tansania", "th": "Thailand", "to": "Tonga", "tt": "Trinidad und Tobago", "tn": "Tunesien", "tm": "Turkmenistan", "tc": "Turks- und Caicosinseln", "tr": "Türkei", "ae": "Vereinigte Arabische Emirate", "ug": "Uganda", "ua": "Ukraine", "gb": "Vereinigtes Königreich", "us": "Vereinigte Staaten", "uy": "Uruguay", "uz": "Usbekistan", "vu": "Vanuatu", "ve": "Venezuela", "vn": "Vietnam", "ye": "Jemen", "zm": "Sambia", "zw": "Simbabwe"]
     
     
     var body: some View {
